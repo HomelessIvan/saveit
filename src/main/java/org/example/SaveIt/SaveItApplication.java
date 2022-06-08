e9eb571f007fd8f7918de970be69a60154c4215e
Ivan Bezdomniy <ivanbezdomniyworking@gmail.com>
2022-06-08 14:11:05 +0300

    initial commit

---
+++ src/main/java/org/example/SaveIt/SaveItApplication.java
@@ -0,0 +1,15 @@
+package org.example.SaveIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveItApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SaveItApplication.class, args);
+                   
+    }
+
+}
---
+++ src/test/java/org/example/SaveIt/SaveItApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.SaveIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
